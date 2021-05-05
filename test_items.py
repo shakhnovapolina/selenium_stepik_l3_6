794928a8ccf51757a59c5183c80a53784fcabbc4
shakhnovapolina <polina.shakhnova@blitzteam.com>
2021-05-05 15:37:49 +0300

    delete sleep(30)

--- test_items.py
+++ test_items.py
@@ -5,7 +5,6 @@ def test_placed_button_add_basket_different_languages(browser, option_lang):
     link = f"http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/"
 
     browser.get(link)
-    time.sleep(30)
     button = browser.find_element_by_class_name("btn-add-to-basket")
     assert button is not None
 
